87c5d7be8bece35477184899bb927a2481e085e6
Monteir0x <msmonteiro@id.uff.br>
2023-06-23 17:41:32 -0300

    first commit

---
+++ src/test/java/com/example/trustme/TrustmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trustme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
